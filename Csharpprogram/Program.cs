2d9be9889640195b94296b1b8a00b75da7e58a73
varsha Gunjal <varshashingote01@gmail.com>
2023-01-13 15:52:24 +0530

    Switch case ex

--- Csharpprogram/Program.cs
+++ Csharpprogram/Program.cs
@@ -9,32 +9,41 @@ namespace Csharpprogram
     internal class Program
     {
         static void Main(string[] args)
-
-
         {
-            Console.WriteLine("welcome to cSharp basic program");
-            Console.WriteLine("Enter first Number");
-            int first = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter second Number");
-            int second= Convert.ToInt32(Console.ReadLine());
-
-            Calculator c1 = new Calculator(first, second);
-            
-            c1.Addition();
-            c1.Substraction();
-            c1.Multiplication();
-
-            c1.division();
-            Console.ReadLine();
+            Console.WriteLine("Welcome to basic program");
+            Console.WriteLine("Please choice any one program");
+            Console.WriteLine("1:Calculator \n 2:WeekDay");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
 
 
+                    Console.WriteLine("welcome to cSharp basic program");
+                    Console.WriteLine("Enter first Number");
+                    int first = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter second Number");
+                    int second = Convert.ToInt32(Console.ReadLine());
 
+                    Calculator c1 = new Calculator(first, second);
 
+                    c1.Addition();
+                    c1.Substraction();
+                    c1.Multiplication();
 
+                    c1.division();
+                    Console.ReadLine();
+                    break;
+                case 2:
 
-
-
-
+                    Console.WriteLine("Enter the Number from 1to 7");
+                    int day = Convert.ToInt32(Console.ReadLine());
+                    WeekDay.Finddaywhichfallonweek(day);
+                    Console.ReadLine();
+                    break;
+            }
         }
     }
 }
+
+         
