d0dc5ffd876f735ad7b8fa1f00d1bc9a70834ad9
varsha Gunjal <varshashingote01@gmail.com>
2023-01-17 16:37:40 +0530

    pattern

---
+++ DemoForLoop/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            /*            j =columns
+                          ******   i=rows
+                          ******
+                          ******
+                          ******
+                          ******
+             
+             */
+            {
+
+
+                for (int i = 1; i <= 5; i++)  //for rows
+                {
+                    for (int j = 1; j <= 5; j++) //for colums
+
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
+
+              
